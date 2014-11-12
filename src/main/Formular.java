e470f697933e5dd05ef93a70a94e25430ce90ddc
Milan Kerslager <milan.kerslager@pslib.cz>
2014-11-12 01:31:15 +0100

    popisy třídy Formular

--- src/main/Formular.java
+++ src/main/Formular.java
@@ -1,19 +1,16 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package main;
 
-/**
- *
- * @author Administrator
- */
+// objekt pro uložení obsahu formuláře
+
 public class Formular {
     
+    // statický seznam stavů osoby (pozor - musí mít obě stejné pořadí)
+    // stavyM - pro muže, stavyZ - ty samé stavy, ale pro ženy
     String stavyM[]={"svobodný", "ženatý", "rozvedený", "vdovec"};
     String stavyZ[]={"svobodná", "vdaná", "rozvedená", "vdova"};
     
+    // export definovaných stavů osoby (ze statického pole)
     public String[] getStavy(boolean muz) {
         if (muz)
             return (stavyM);
